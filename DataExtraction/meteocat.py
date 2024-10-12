94b687b929312bb4c366ad0c1ae0de118c8af188
Arnau Sanz <60817562+arnausanz@users.noreply.github.com>
2024-10-12 19:52:49 +0200

    test_auto_run

--- DataExtraction/meteocat.py
+++ DataExtraction/meteocat.py
@@ -1,18 +1,38 @@
+import json
+import os
+
 import requests
 import pandas as pd
-from utils import calling_api_message, set_meteocat_api_key
+from utils import calling_api_message, set_meteocat_api_key, save_df_to_csv
 
-__HEADERS = {"Content-Type": "application/json", "X-Api-Key": "__KEY__"}
+"""
+This script is used to extract daily data from Meteocat API.
+In the metadata files can be found the variable codes and station codes.
+"""
+
+__HEADERS = {"Content-Type": "application/json", "X-Api-Key": set_meteocat_api_key()}
 __XEMA_BASE_URL = "https://api.meteo.cat/xema/v1/"
 __DAILY_DATA_URL = [__XEMA_BASE_URL + "variables/estadistics/diaris/", "?codiEstacio=", "&any=", "&mes="]
+__VARIABLE_FOR_ALL_STATIONS = ["variables/mesurades/{var_code}/{year}/{month}/{day}", "?codiEstacio={station_code}"]
 
 def get_daily_data(var_code = None, station_code = None, year = None, month = None) -> pd.DataFrame:
+    """
+    OBSOLETE --> Won't use this function as we can get all the data from one variable from the API in one call
+    """
     url = __DAILY_DATA_URL[0] + var_code + __DAILY_DATA_URL[1] + station_code + __DAILY_DATA_URL[2] + year + __DAILY_DATA_URL[3] + month
-    __HEADERS['X-Api-Key'] = set_meteocat_api_key()
     response = requests.get(url, headers=__HEADERS)
     calling_api_message('Meteocat', 'Daily data from station: ' + station_code + ' - Year: ' + year + ' Month: ' + month + ' - Var: ' + var_code)
     print('Response status code: ' + str(response.status_code))
     data_df = pd.json_normalize(response.json(), record_path=['valors'], meta=['codiEstacio', 'codiVariable'])
     return data_df
 
-print(get_daily_data('1300', 'UG', '2024', '01'))
+def get_all_stations_data_per_day_and_var(var_code = None, year = None, month = None, day = None, station_code = None) -> pd.DataFrame:
+    url = __XEMA_BASE_URL + __VARIABLE_FOR_ALL_STATIONS[0].format(var_code=var_code, year=year, month=month, day=day)
+    url += __VARIABLE_FOR_ALL_STATIONS[1].format(station_code=station_code) if station_code is not None else ''
+    response = requests.get(url, headers=__HEADERS)
+    calling_api_message('Meteocat', 'Daily data from all stations - Year: ' + year + ' Month: ' + month + ' - Var: ' + var_code)
+    print('Response status code: ' + str(response.status_code))
+    data = pd.json_normalize(response.json(), record_path=['variables', 'lectures'], meta=['codi'])
+    return data
+
+save_df_to_csv(get_all_stations_data_per_day_and_var('35', '2024', '09', '21'), "test5")
--- DataExtraction/utils.py
+++ DataExtraction/utils.py
@@ -2,6 +2,7 @@ import os
 from ast import parse
 from datetime import datetime
 
+import pandas as pd
 from dotenv import load_dotenv
 
 def calling_api_message(api_name, more_info = None):
@@ -15,3 +16,8 @@ def set_meteocat_api_key() -> str:
 
 def parse_date(date: str) -> datetime:
     return datetime.strptime(date, '%Y-%m-%dZ')
+
+def save_df_to_csv(df, file_name, path = 'Data/') -> None:
+    project_dir = os.path.dirname(os.path.dirname(__file__))
+    _path = os.path.join(project_dir, path)
+    df.to_csv(_path + file_name, index=False)
