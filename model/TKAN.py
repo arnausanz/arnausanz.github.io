7fb1d37189ce5ba8aed9e6791f3fb75edb2adc5a
Arnau Sanz <60817562+arnausanz@users.noreply.github.com>
2024-11-06 20:02:12 +0100

    Change python version and run TKAN in notebook check

--- model/TKAN.py
+++ model/TKAN.py
@@ -2,19 +2,22 @@ import keras
 from tkan import TKAN
 import numpy as np
 
-# Create fake X and y data
-X_train_seq = np.random.rand(100, 10, 5)
-y_train_seq = np.random.rand(100, 5)
 
 class Model:
-      model = keras.Sequential([
-            keras.layers.InputLayer(shape=X_train_seq.shape[1:]),
-            TKAN(100, sub_kan_configs=[{'spline_order': 3, 'grid_size': 10}, {'spline_order': 1, 'grid_size': 5}, {'spline_order': 4, 'grid_size': 6}, ], return_sequences=True, use_bias=True), #Define the params of the KANLinear as dict as here
-            TKAN(100, sub_kan_configs=[1, 2, 3, 3, 4], return_sequences=True, use_bias=True), #Use float or int to specify only the exponent of the spline
-            TKAN(100, sub_kan_configs=['relu', 'relu', 'relu', 'relu', 'relu'], return_sequences=True, use_bias=True), #Or use string to specify the standard tensorflow activation using Dense in sublayers instead of KANLinear
-            TKAN(100, sub_kan_configs=[None for _ in range(3)], return_sequences=False, use_bias=True), # Or put None for default activation
-            keras.layers.Dense(y_train_seq.shape[1]),
-      ])
+      def __init__(self):
+            # Create fake X and y data
+            self.X_train_seq = np.random.rand(100, 10, 5)
+            self.y_train_seq = np.random.rand(100, 5)
 
-      model.compile(optimizer='adam', loss='mse')
-      model.fit(X_train_seq, y_train_seq, epochs=10, verbose=1)
+            self.model = keras.Sequential([
+                  keras.layers.InputLayer(shape=self.X_train_seq.shape[1:]),
+                  TKAN(100, sub_kan_configs=[{'spline_order': 3, 'grid_size': 10}, {'spline_order': 1, 'grid_size': 5}, {'spline_order': 4, 'grid_size': 6}, ], return_sequences=True, use_bias=True), #Define the params of the KANLinear as dict as here
+                  TKAN(100, sub_kan_configs=[1, 2, 3, 3, 4], return_sequences=True, use_bias=True), #Use float or int to specify only the exponent of the spline
+                  TKAN(100, sub_kan_configs=['relu', 'relu', 'relu', 'relu', 'relu'], return_sequences=True, use_bias=True), #Or use string to specify the standard tensorflow activation using Dense in sublayers instead of KANLinear
+                  TKAN(100, sub_kan_configs=[None for _ in range(3)], return_sequences=False, use_bias=True), # Or put None for default activation
+                  keras.layers.Dense(self.y_train_seq.shape[1]),
+            ])
+
+      def compile_execute_test(self):
+            self.model.compile(optimizer='adam', loss='mse')
+            self.model.fit(self.X_train_seq, self.y_train_seq, epochs=10, verbose=1)
