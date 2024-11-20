0463b653b0e5b369259a892f6ff1c760499f0be1
Arnau Sanz <60817562+arnausanz@users.noreply.github.com>
2024-11-20 20:23:52 +0100

    simplify main.py

--- DataExtraction/main.py
+++ DataExtraction/main.py
@@ -1,14 +1,19 @@
-# This document will be the main script for data extraction (automatically).
 from datetime import datetime
 import aca
 import meteocat as m
 import utils
 
-log_trigger = 'Manual'
+"""
+This script updates the data from ACA and Meteocat and saves it to the corresponding files.
+Used to update automatically the data every day.
+Can be executed manually to update the data (change the log_trigger to 'Manual') or automatically by the scheduler.
+"""
+
+_log_trigger = 'Manual'
 
 # Update data from ACA
 print('Updating ACA data...')
-aca.join_daily_data_with_all_data(aca.transform_daily_data(aca.get_daily_data()), overwrite=True, log_trigger=log_trigger)
+aca.join_daily_data_with_all_data(aca.transform_daily_data(aca.get_daily_data()), overwrite=True, log_trigger=_log_trigger)
 print('ACA data updated successfully')
 
 # Update data from Meteocat
@@ -23,7 +28,7 @@ current_month = "1" if current_month == 0 else str(current_month)
 current_year = str(datetime.now().year)
 for var in meteocat_vars:
     print(f'Updating Meteocat data for variable {var}...')
-    m.log_meteocat_data(var, current_year, current_month, trigger=log_trigger)
+    m.log_meteocat_data(var, current_year, current_month, trigger=_log_trigger)
     today_values = m.add_today_information(today_vars[var])
     daily_values = m.transform_daily_data(m.get_daily_data(var, current_year, current_month))
     merged_values = m.join_daily_and_today_data(daily_values, today_values)
