d5f72999adab5c0498a89a3e133114a49b35b72d
Arnau Sanz <60817562+arnausanz@users.noreply.github.com>
2024-11-20 21:40:33 +0100

    update requirements.txt

--- model/LSTM.py
+++ model/LSTM.py
@@ -1,7 +1,6 @@
 import torch
 import torch.nn as nn
 from prepare_data import get_data_prepared
-from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error, mean_squared_error
 import numpy as np
 import matplotlib.pyplot as plt
