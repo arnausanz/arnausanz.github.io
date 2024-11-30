ffb64de0962d2eedfabe5cf5fe77af8e5ce5670f
Arnau Sanz <60817562+arnausanz@users.noreply.github.com>
2024-11-30 15:36:06 +0100

    delete old files

--- model/old_LSTM.py
+++
@@ -1,8 +0,0 @@
-import torch
-import torch.nn as nn
-
-# Read model from /Users/arnausanz/Library/Mobile Documents/com~apple~CloudDocs/Documents/Estudis/UOC/Master Data Science/Semestre 7/TFM/TFM_arnausanz/model/final_models/model_1/model/model.pth
-model = nn.Module()
-model.load_state_dict(torch.load('/Users/arnausanz/Library/Mobile Documents/com~apple~CloudDocs/Documents/Estudis/UOC/Master Data Science/Semestre 7/TFM/TFM_arnausanz/model/final_models/model_1/model/model.pth'))
-
-print(model)
