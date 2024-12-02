8d592be93f43521c41991370355a4d30d2a04278
Arnau Sanz <60817562+arnausanz@users.noreply.github.com>
2024-12-02 23:04:02 +0100

    new model class and updated both models

--- xLSTM.py
+++ xLSTM.py
@@ -1,12 +1,16 @@
 from xlstm import xLSTMBlockStackConfig, mLSTMBlockConfig, mLSTMLayerConfig, sLSTMBlockConfig, sLSTMLayerConfig, FeedForwardConfig
+from model.model import Model, ModelConfig, save_model, load_model, get_split_data
+
+
+X_train_x, X_test_x, y_train_x, y_test_x, scalers_x = get_split_data('LSTM',90)
 
 config = xLSTMBlockStackConfig(
     mlstm_block=mLSTMBlockConfig(mlstm=mLSTMLayerConfig(conv1d_kernel_size=4, qkv_proj_blocksize=4, num_heads=4)),
     slstm_block=sLSTMBlockConfig(slstm=sLSTMLayerConfig(backend="vanilla", num_heads=4, conv1d_kernel_size=2,
                                                         bias_init="powerlaw_blockdependent"),
                                  feedforward=FeedForwardConfig(proj_factor=2, act_fn="gelu")),
-    context_length=X.shape[1],
+    context_length=X_train_x.shape[1],
     num_blocks=3,
-    embedding_dim=X.shape[2],
+    embedding_dim=X_train_x.shape[2],
     slstm_at=[1]
 )
